fc28d0b48967e248aa6ddaecde06db43644a6fc3
alepape1 <alesantanag@gmail.com>
2024-10-15 16:35:21 +0100

    Aplicacion con GUI funcionando

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
+from tkinter import messagebox
 from video_processing import VideoProcessor
 import os
 import cv2  # Importa OpenCV aquí
@@ -13,40 +14,81 @@ class VideoGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Procesador de Videos")
-        self.root.geometry("800x600")
+        self.root.geometry("900x800")
 
-        # Progreso y estado
-        self.progress_var = tk.DoubleVar()
-        self.status_label = tk.Label(root, text="Selecciona la carpeta de videos")
-        self.status_label.pack(pady=10)
+        # Frame superior para los botones
+        top_frame = tk.Frame(self.root, borderwidth=3, relief="groove", padx=10, pady=10)
+        top_frame.pack(side=tk.TOP, anchor=tk.W, padx=10, pady=30)
 
-        self.progress_bar = ttk.Progressbar(root, variable=self.progress_var, maximum=100)
-        self.progress_bar.pack(pady=10, padx=10, fill=tk.X)
+        # Botones en el frame superior
+        self.select_button = tk.Button(top_frame, text="Seleccionar carpeta", command=self.select_folder)
+        self.select_button.pack(side=tk.LEFT, padx=5)
 
-        # Label para mostrar el video
-        self.video_label = tk.Label(root)
-        self.video_label.pack(pady=10, padx=10)
+        self.open_button = tk.Button(top_frame, text="Archivos procesados", command=self.open_output_folder)
+        self.open_button.pack(side=tk.LEFT, padx=5)
 
-        # Label para mostrar el nombre del video
-        self.video_name_label = tk.Label(root, text="")
-        self.video_name_label.pack(pady=10)
+        self.process_button = tk.Button(top_frame, text="Procesar Videos", state="disabled", command=self.process_videos)
+        self.process_button.pack(side=tk.LEFT, padx=5)
+
+     
 
         # Checkbox para mostrar video
-        self.show_video_var = tk.BooleanVar(value=True)  # Valor inicial a True (mostrar video)
-        self.show_video_checkbox = tk.Checkbutton(root, text="Mostrar video", variable=self.show_video_var)
-        self.show_video_checkbox.pack(pady=10)
+        self.show_video_var = tk.BooleanVar(value=True)
+        self.show_video_checkbox = tk.Checkbutton(top_frame, text="Mostrar video", variable=self.show_video_var)
+        self.show_video_checkbox.pack(side=tk.LEFT, padx=5)
 
-        # Botones
-        self.select_button = tk.Button(root, text="Seleccionar carpeta", command=self.select_folder)
-        self.select_button.pack(pady=10)
+        # Frame inferior para el resto de elementos
+        bottom_frame = tk.Frame(self.root, borderwidth=3, relief="groove", padx=10, pady=10)
+        bottom_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-        self.open_button = tk.Button(root, text="Archivos procesados", command=self.open_output_folder)
-        self.open_button.pack(pady=10)
+        # Label para mostrar el nombre del video
+        self.video_name_label = tk.Label(bottom_frame, text="")
+        self.video_name_label.pack(pady=5)
 
-        self.process_button = tk.Button(root, text="Procesar Videos", state="disabled", command=self.process_videos)
-        self.process_button.pack(pady=10)
+        # Label para mostrar el video
+        self.video_label = tk.Label(bottom_frame)
+        self.video_label.pack(pady=5)
+
+        # Status label
+        self.status_label = tk.Label(bottom_frame, text="Selecciona la carpeta de videos")
+        self.status_label.pack(pady=10,padx=10)
+
+       
+        # Frame para la barra de progreso y el contador
+        progress_frame = tk.Frame(bottom_frame)
+        progress_frame.pack(fill=tk.X, pady=10, padx=20)
+
+        # Botón para cerrar y salir
+        self.close_button = tk.Button(progress_frame, text="Cerrar y Salir", command=self.close_and_quit)
+        self.close_button.pack(side=tk.LEFT, padx=5)
+
+        # Barra de progreso
+        self.progress_var = tk.DoubleVar()
+        self.progress_bar = ttk.Progressbar(progress_frame, variable=self.progress_var)
+        self.progress_bar.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+        # Contador
+        self.counter_var = tk.DoubleVar()
+        self.counter_label = tk.Label(progress_frame, textvariable=self.counter_var , width=10)
+        self.counter_label.pack(side=tk.LEFT, padx=(10, 0))  # Añade un poco de espacio a la izquierda
 
         self.folder_path = ""
+
+    def close_and_quit(self):
+        # Mostrar un cuadro de diálogo de confirmación
+        if messagebox.askyesno("Confirmar salida", "¿Estás seguro de que quieres cerrar la aplicación y parar el proceso de analisis del video?"):
+            if hasattr(self, 'processor'):
+                self.processor.stop_processing()
+                # Esperar un poco para que los hilos se detengan
+                self.root.after(1000, self.force_quit)
+            else:
+                self.force_quit()
+        # Si el usuario selecciona "No", simplemente retorna y no hace nada
+    
+    def force_quit(self):
+
+        self.root.quit()
+        self.root.destroy()
 
     def open_output_folder(self):
         """Abrir la carpeta de salida en el explorador de archivos."""
@@ -68,10 +110,12 @@ class VideoGUI:
             self.process_button.config(state="normal")
 
     def update_progress(self, progress):
+        self.counter_var.set(f"{int(progress)}%")
         self.progress_var.set(progress)
         self.root.update_idletasks()
 
     def update_video_frame(self, frame):
+
         if frame is not None and self.show_video_var.get():  # Verifica si se debe mostrar el video
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image = Image.fromarray(frame)
@@ -86,14 +130,14 @@ class VideoGUI:
 
     def process_videos(self):
         if self.folder_path:
-            processor = VideoProcessor(
+            self.processor = VideoProcessor(
                 self.folder_path, 
                 self.update_progress, 
                 self.update_video_frame, 
                 self.update_video_name,  # Pasar el callback para actualizar el nombre del video
                 root=self.root  # Pasar root para usar after en el VideoProcessor
             )
-            processor.process_videos()
+            self.processor.process_videos()
             self.status_label.config(text="Procesando videos...")
 
 
--- video_processing.py
+++ video_processing.py
@@ -2,14 +2,15 @@ import os
 import cv2
 from threading import Thread
 from motion_detection import detect_motion  # Asegúrate de que esta importación funcione correctamente
-from utils import save_video_segment, write_file_txt  # Asegúrate de que estas funciones existan
 import tkinter as tk  # Asegúrate de importar tkinter
-
+import os
 
 class VideoProcessor:
 
     def __init__(self, folder_path, update_progress_callback=None, update_video_frame_callback=None, 
                  update_video_name_callback=None, completed_files_listbox=None, root=None):
+                
+        self.running = True  # Añade esta variable de control
         self.folder_path = folder_path
         self.update_progress = update_progress_callback
         self.update_video_frame = update_video_frame_callback
@@ -21,16 +22,49 @@ class VideoProcessor:
         if not os.path.exists("output"):
             os.makedirs("output")
 
+    #Funcion para guardar el video resumen
+    def save_video_segment(self,out, cap, start_time, end_time):
+
+
+        # Asegurarse de que el start_time nunca sea menor que 0
+        start_time = max(0, start_time - 1)  # Grabar 1 segundo antes
+        end_time = end_time + 1  # Grabar 1 segundo después
+
+        # Saltar al frame de inicio ajustado (1 segundo antes)
+        cap.set(cv2.CAP_PROP_POS_MSEC, start_time * 1000)
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            # Obtener el tiempo actual en segundos
+            current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
+            
+            # Detener cuando el tiempo actual es mayor que el end_time ajustado (1 segundo después)
+            if current_time > end_time:
+                break
+            
+            out.write(frame)
+
+
     def process_video(self, video_path):
         
+        print(f"The video path is: {video_path}")
+
         try:
             cap = cv2.VideoCapture(video_path)
             if not cap.isOpened():
                 print(f"No se pudo abrir el video: {video_path}")
                 return
-
+         
             video_file_name = os.path.splitext(os.path.basename(video_path))[0]
             output_path = f"output/{video_file_name}_resumen.avi"
+
+            # Si el archivo existe, eliminarlo antes de crear el nuevo segmento de video
+            if os.path.exists(output_path):
+                os.remove(output_path)
+            
             fps = cap.get(cv2.CAP_PROP_FPS)
             frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -42,30 +76,55 @@ class VideoProcessor:
 
             total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
             fish_detected = False  # Bandera para indicar si se ha detectado un pez
+            
+              # Initialize variables for this video
+            fish_count = 0
+            detected_fish = []
+            start_time = None
+            start_times = []
+            end_times = []
+            min_scene_duration = 1  # Minimum 1 seconds for a scene to be saved
+
 
             # Procesar cada fotograma
             for current_frame in range(total_frames):
+           
                 ret, frame = cap.read()
                 if not ret:
                     print(f"Fin del video o error al leer el fotograma: {current_frame}")
                     break
 
-                fish_count = detect_motion(frame, [], 0)
+                if not self.running:
+                    print(f"Cancelado por el usuario: {current_frame}")
+                    break
 
-                # Calcular y actualizar el progreso del video
-                progress = (current_frame / total_frames) * 100
+                # Lógica de detección de movimiento y conteo de peces aquí
+                fish_counter = detect_motion(frame, detected_fish, fish_count)
+                if fish_counter > 0 and start_time is None:
+                    start_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
+                elif fish_counter == 0 and start_time is not None:
+                    end_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
+                    start_times.append(start_time)
+                    end_times.append(end_time)
+                    start_time = None
+                    # Calcular y actualizar el progreso del video
+                progress = round((current_frame / total_frames) * 100,1)
+                # Mostrar el progreso en la consola
+                print(f"Procesando {video_file_name}: {progress:.2f}% completado", end="\r")
                 if self.update_progress:
                     self.root.after(0, self.update_progress, progress)
-
-                # Guardar el frame en el video de salida
-                out.write(frame)  # Guarda todos los frames para mantener la velocidad original
-
                 # Actualizar el frame en la GUI si es necesario
                 if self.update_video_frame:
                     self.root.after(0, self.update_video_frame, frame)
 
-                if cv2.waitKey(30) & 0xFF == ord('q'):
-                    break
+
+            # Guardar todos los segmentos en el video resumen
+            for start_time, end_time in zip(start_times, end_times):
+                
+                # Add condition to check if the scene is longer than the minimum duration
+                if end_time - start_time > min_scene_duration:
+                    self.save_video_segment(out, cap, start_time, end_time)
+
 
             cap.release()
             out.release()  # Asegúrate de liberar el VideoWriter
@@ -85,6 +144,8 @@ class VideoProcessor:
 
         def process_thread():
             for i, video_file in enumerate(video_files, 1):
+                if not self.running:
+                    break
                 video_path = os.path.join(self.folder_path, video_file)
                 self.process_video(video_path)
 
@@ -93,3 +154,7 @@ class VideoProcessor:
 
         thread = Thread(target=process_thread)
         thread.start()
+
+
+    def stop_processing(self):
+        self.running = False  # Esto señalizará a los hilos que deben detenerse
